a9e082619278c007d83da4c87802e9cc095a2a8b
99-Malik <74320681+99-Malik@users.noreply.github.com>
2024-11-23 17:20:27 +0500

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,8 @@ import companyName from "../libs/data";
 import { Raleway } from "next/font/google";
 import { cn } from "@/lib/utils";
 import FixedCallButtons from "@/components/buttons/FixedCallButtons";
+import Script from "next/script";
+
 
 const font = Raleway({
   subsets: ["latin"],
@@ -16,6 +18,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <head>
+        {/* Include gtag.js script */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-16717207756" />
+
+        {/* Include gtag.js initialization script */}
+        <Script dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config','AW-16717207756');
+        ` }} />
+
+
+      </head>
+    
       <body className={cn(font.className, "flex flex-col w-full")}>
         {children}
         <FixedCallButtons />
